c8b26f1dcd82c3964adb95305971dc4ff62ef293
liruirui6174 <527268131@qq.com>
2019-04-30 15:08:59 +0800

    Update myHandler.java

--- src/com/lirui/socket/handlerImpl/myHandler.java
+++ src/com/lirui/socket/handlerImpl/myHandler.java
@@ -32,6 +32,6 @@ public class myHandler extends ChatWebSocketHandler {
 	    	   e.printStackTrace();
 	       }
 	      
-	        session.sendMessage(message);
+	        session.sendMessage(new TextMessage(is.getTest().getProperty_A()));
 	    }
 }
